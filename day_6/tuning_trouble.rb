f287ba85f0d9552f876646336f7ec9788c1a8e3a
Mr-Bowtie <mr.bowtie989@gmail.com>
2022-12-06 06:18:24 -0500

    Day 6: first puzzle - complete

---
+++ day_6/tuning_trouble.rb
@@ -0,0 +1,16 @@
+# No planning today, just speed!
+# completed in 8:18.26 (not leaderboard worthy, but im still pleased)
+require 'pry'
+data_stream = File.read('data_stream.txt')
+
+counter = 1
+loop do
+  packet = data_stream[(counter - 1), 4]
+  break if packet.split('').uniq.length == 4
+
+  counter += 1
+end
+
+# Counter marks the start of the packet
+# the puzzle is looking for the total number of characters processed, so we want the location of the end of the packet
+puts counter + 3
