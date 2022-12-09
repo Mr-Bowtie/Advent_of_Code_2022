ce6547b63906e82efe37e57ac080964a3b88ede2
Mr-Bowtie <mr.bowtie989@gmail.com>
2022-12-08 20:51:19 -0500

    Day 8: second puzzle - complete

--- day_8/tree_house_planning.rb
+++ day_8/tree_house_planning.rb
@@ -1,36 +1,34 @@
-# frozen_string_literal: true
-
 # INPUT:
 #   - a grid of integers
 #   - each int represents the height of a tree in that position
 #
 # OBJECTIVE:
-#   - count how many trees are visible from outside the grid
+#   - determine the highest scenic score for a single tree in the grid
 #
 # RULES:
-#   - all tree along the edges are considered visible
-#   - a tree is visible if all other trees between it and an edge are shorter than it
+#   - scenic score found by multiplying together the viewing distance in each of the four directions
+#   - view distance in one direction is the number of trees between(and including) the current tree and either an edge or a tree of equal or greater height
+#   - a tree on the edge has viewing distance of 0 in the direction of the edge
 #   - the grid is a square 99x99
 #
 # ALGO:
-#   - split file by \n
-#   - generate columns and rows hashes
-#     - iterate over split line
-#       - append each int(convert from string to int) to an array whose key is the ints position
-#       - same but for row position
-#   - for all rows
-#     - for each int
-#       - starting ints are all visible
-#       - determine if any tree is taller from tree_start to end of the row
-#       - reverse row, check again
-#       - add position [row, col] of any visible trees to visible_trees array
-#         - only add position if it's inverse doesnt already exists in the array ([0,1] [1,0])
-#   - the same but for columns
-#   - count the number of elements in visible_trees
+#
+#   - for each int
+#     - find left/right viewing distance(rows)
+#       - create two arrays
+#         - tree_pos..
+#         - (0...tree_pos).reverse
+#       - each_with_index
+#         - break if el >= tree
+# =>      - return index + 1
+#     - find up/down viewing distance(cols)
+#     - determine scenic score
 #
 require 'pry'
+require 'matrix'
+
 class TreeGrid
-  attr_accessor :tree_grid, :rows, :columns, :visible_trees
+  attr_accessor :tree_grid, :rows, :columns, :visible_trees, :most_scenic_tree, :tree_scores
 
   def initialize
     @tree_grid = File.read('tree_heights.txt').split("\n")
@@ -38,6 +36,8 @@ class TreeGrid
     @columns = {}
     build_grid
     @visible_trees = spot_tall_trees
+    @tree_scores = {}
+    @most_scenic_tree = calc_scenic_tree
   end
 
   def build_grid
@@ -98,7 +98,49 @@ class TreeGrid
     end
     talls
   end
+
+  # returns viewing distances in one direction pair - [front, back] or [up, down]
+  def determine_viewing_distance(tree_pos, sight_line_vals)
+    # left or up
+    return [0, 0] if tree_pos == 0
+
+    tree = sight_line_vals[tree_pos]
+    in_front = sight_line_vals[0...tree_pos].reverse
+    behind = sight_line_vals[(tree_pos + 1)..]
+    viewing_distances = []
+
+    in_front.each_with_index do |distant_tree, idx|
+      distance = idx + 1
+      if distant_tree >= tree || distance == in_front.length
+        viewing_distances.push(distance)
+        break
+      end
+    end
+
+    behind.each_with_index do |distant_tree, idx|
+      distance = idx + 1
+      next unless distant_tree >= tree || distance == behind.length
+
+      viewing_distances.push(distance)
+      break
+    end
+    viewing_distances
+  end
+
+  def calc_scenic_tree
+    grid = Matrix.build(98) { |row, col| [row, col] }.to_a.flatten(1)
+    grid.each do |row, col|
+      # rows
+      score_tally = determine_viewing_distance(col, rows[row])
+      # columns
+      score_tally += determine_viewing_distance(row, columns[col])
+
+      tree_scores[[row, col]] = score_tally.reduce(&:*)
+    end
+
+    tree_scores.values.reduce(0) { |value, memo| value > memo ? memo = value : memo }
+  end
 end
 
 tree_field = TreeGrid.new
-puts tree_field.visible_trees.count
+puts tree_field.most_scenic_tree
